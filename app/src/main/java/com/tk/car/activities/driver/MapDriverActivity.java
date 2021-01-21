17f7deb888fce5457065e35a2563d2730e025547
Stranger01 <joseantoniosysnet@gmail.com>
2021-01-20 20:52:14 -0500

    map driver, mapa actualizado

--- app/src/main/java/com/tk/car/activities/driver/MapDriverActivity.java
+++ app/src/main/java/com/tk/car/activities/driver/MapDriverActivity.java
@@ -7,34 +7,49 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.tk.car.R;
 import com.tk.car.activities.LoginActivity;
 import com.tk.car.providers.AuthProvider;
 
-public class MapDriverActivity extends AppCompatActivity {
+public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    Button logOut;
-    AuthProvider mAuthProvider;
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
 
-        logOut = findViewById(R.id.logOut);
-        mAuthProvider = new AuthProvider();
+//        mBtnConnect = findViewById(R.id.btnConnect);
+//        mAuthProvider = new AuthProvider();
 
-        logOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
 
-                mAuthProvider.logout();
 
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                finish();
+//        logOut.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                mAuthProvider.logout();
+//
+//                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//
+//            }
+//        });
+    }
 
-            }
-        });
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
     }
 }
