98bad846130beea07cedb7273767ade76abc7949
Stranger01 <joseantoniosysnet@gmail.com>
2021-02-25 20:27:32 -0500

    primeros pasos en JetPack

---
+++ app/src/main/java/com/example/jetpack/Main.kt
@@ -0,0 +1,12 @@
+package com.example.jetpack
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun HolaMundo(){
+    //Text(text = "hola desde la clase Kt")
+
+}
--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -1,27 +1,63 @@
 package com.example.jetpack
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpack.ui.theme.JetPackTheme
+import com.example.jetpack.ui.theme.Purple200
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MyApp {
 
-            Text(text = "Hola Mundo desde JetPAck :)")
+                HolaMundos()
+                Divider(color = Color.Black)
+                Text(text = "Prueba 1")
+                Divider(color = Color.Black)
+                Text(text = "Prueba 2")
+                Divider(color = Color.Black)
+                Text(text = "Prueba 3")
 
+            }
         }
+    }
 
+    @Composable
+    fun HolaMundos() {
+
+
+            Text(text = "hola", fontSize = 25.sp)
+
+    }
+
+    @Composable
+    fun MyApp(content: @Composable() () -> Unit) {
+
+        JetPackTheme() {
+            Surface(color = Color.Cyan) {
+
+                Column() {
+                    content()
+                }
+            }
+        }
     }
 }
   
